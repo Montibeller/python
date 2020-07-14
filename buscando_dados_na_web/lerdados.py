f025022e1e6e1ba171da5ceee868d5bbf554f697
Eric Montibeller <monttibeller@live.com>
2020-07-13 21:07:16 -0300

    Iniciando os estudos

---
+++ buscando_dados_na_web/lerdados.py
@@ -0,0 +1,14 @@
+import investpy as inv
+import pandas as pd
+
+# fazendo uma listagem de todas as ações no banco de dados
+acoes_br = inv.get_stocks('brazil')
+print(acoes_br)
+
+# buscando uma descrição de uma ação
+""" acoes_br = inv.get_stock_company_profile('USIM5', country='brazil')
+print(acoes_br) """
+
+# baixando a contação hitorica
+""" usim5 = inv.get_stock_historical_data('USIM5', country='brazil', from_date='01/01/1900', to_date='11/07/2020')
+print(usim5) #posso acrecentar ['nome da coluna'] para exibir so a conluna """
