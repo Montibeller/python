0031604ec59884e3958034a4267bf33164e52bdc
Eric Montibeller <monttibeller@live.com>
2020-07-20 23:09:08 -0300

    criação da tabela dos preços das acoes

--- bucando_acoes_salva_banco/bucando_e_salvando.py
+++ bucando_acoes_salva_banco/bucando_e_salvando.py
@@ -5,11 +5,22 @@ import sqlite3
 connection = sqlite3.connect('banco.db')
 c = connection.cursor()
 
-def create_tabela():
-    c.execute('CREATE TABLE IF NOT EXISTS acoes(\
-        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, \
-        cod TEXT UNIQUE NOT NULL, \
-        nome TEXT NOT NULL)')
-create_tabela()
+def create_tabela_acoes():
+    c.execute('''CREATE TABLE IF NOT EXISTS acoes(
+			        id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+			        cod TEXT UNIQUE NOT NULL,
+			        nome TEXT NOT NULL)''')
+create_tabela_acoes()
+
+def create_tabela_cotacoes():
+	c.execute('''CREATE TABLE IF NOT EXISTS cotacoes(
+					id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+					acao INTEGER,
+					abertura REAL,
+					fechamento REAL,
+					maxima REAL,
+					minima REAL,
+					volume REAL,)''')
+create_tabela_cotacoes()
 
 #acoes_br = inv.get_stocks_list('brazil')
