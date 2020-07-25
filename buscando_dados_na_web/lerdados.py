47f8209e25b7a0497ba5d8603e4fd38945cc54dd
Eric Montibeller <monttibeller@live.com>
2020-07-25 10:42:28 -0300

    piquenas mudanças no codigo

--- buscando_dados_na_web/lerdados.py
+++ buscando_dados_na_web/lerdados.py
@@ -1,12 +1,11 @@
 import investpy as inv
-import pandas as pd
+
+acoes_br = inv.get_stocks_list('brazil')
+print(acoes_br)
 
 # fazendo uma listagem de todas as ações no banco de dados
 """ acoes_br = inv.get_stocks('brazil')
 print(acoes_br) """
-
-acoes_br = inv.get_stocks_list('brazil')
-print(acoes_br)
 
 # buscando uma descrição de uma ação
 """ acoes_br = inv.get_stock_company_profile('USIM5', country='brazil')
@@ -15,3 +14,4 @@ print(acoes_br) """
 # baixando a contação hitorica
 """ usim5 = inv.get_stock_historical_data('USIM5', country='brazil', from_date='01/01/1900', to_date='11/07/2020')
 print(usim5) #posso acrecentar ['nome da coluna'] para exibir so a conluna """
+
